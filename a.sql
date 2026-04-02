2524b829d66e02059b92c1b64f8c31d91066b961
ballonde <93644915+ballonde@users.noreply.github.com>
2026-04-02 11:59:57 +0200

    la bd

---
+++ a.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : jeu. 02 avr. 2026 à 09:59
+-- Version du serveur : 5.7.24
+-- Version de PHP : 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `ds_bdd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `a`
+--
+-- Erreur de lecture de structure pour la table ds_bdd.a : #1146 - La table &#039;ds_bdd.a&#039; n&#039;existe pas
+-- Erreur de lecture des données pour la table ds_bdd.a : #1064 - Erreur de syntaxe près de &#039;FROM `ds_bdd`.`a`&#039; à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
